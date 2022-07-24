069a9c3c2f9f032a8e2ff469e9ecec1a01a84ea0
arshirazi97 <90085179+arshirazi97@users.noreply.github.com>
2022-07-24 20:46:50 +0500

    Initail Commit

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,34 @@
+import { createContext, useReducer } from 'react'
+
+export const ThemeContext = createContext()
+
+const themeReducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE_COLOR':
+        return {...state, color: action.payload}
+        default: 
+        case 'CHANGE_MODE':
+        return {...state, mode: action.payload}
+        return state
+    }
+}
+
+export function ThemeProvider({ children }) {
+    const [state, dispatch] = useReducer(themeReducer, {
+        color: '#58249c',
+        mode: 'dark'
+    })
+
+    const changeColor = (color) => {
+        dispatch({ type: 'CHANGE_COLOR', payload: color})
+    }
+    const changeMode = (mode) => {
+        dispatch({type: 'CHANGE_MODE', payload: mode})
+    } 
+    
+    return (
+        <ThemeContext.Provider value={{ ...state, changeColor, changeMode }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
